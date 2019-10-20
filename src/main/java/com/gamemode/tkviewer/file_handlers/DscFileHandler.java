8ef0c83d6c4336ecb56f9fed424d0524594eb7d5
Reid Castner <reidcastner@gmail.com>
2019-10-20 15:35:15 -0400

    Adding Parts Animation rendering

--- src/main/java/com/gamemode/tkviewer/file_handlers/DscFileHandler.java
+++ src/main/java/com/gamemode/tkviewer/file_handlers/DscFileHandler.java
@@ -1,6 +1,8 @@
 package com.gamemode.tkviewer.file_handlers;
 
 import com.gamemode.tkviewer.resources.Part;
+import com.gamemode.tkviewer.resources.PartBlock;
+import com.gamemode.tkviewer.resources.PartChunk;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -29,29 +31,51 @@ public class DscFileHandler extends FileHandler {
         this.parts = new ArrayList<Part>();
         for (int i = 0; i < this.partCount; i++) {
             long id = this.readInt(true, true);
-            long paletteIndex = this.readInt(true,true);
+            long paletteIndex = this.readInt(true, true);
 
-            long frameIndex = this.readInt(true,true);
-            long frameCount = this.readInt(true,true);
-
-            this.parts.add(new Part(id, paletteIndex, frameIndex, frameCount));
+            long frameIndex = this.readInt(true, true);
+            long frameCount = this.readInt(true, true);
 
             // Unknown Flags
-            this.seek(14, false);
+            int partUnknown1 = this.readShort(true, false);
+            int partUnknown2 = this.readShort(true, false);
+            int partUnknown3 = this.readShort(true, false);
+            int partUnknown4 = this.readShort(true, false);
+            int partUnknown5 = this.readShort(true, false);
+            int partUnknown6 = this.readShort(true, false);
+            int partUnknown7 = this.readShort(true, false);
+
+
 
             // Number of Chunks
             long chunkCount = this.readInt(true, true);
+            List<PartChunk> partChunks = new ArrayList<>();
             for (int j = 0; j < chunkCount; j++) {
                 // Two Unknown Ints
-                this.seek(8, false);
+                long chunkNumber = this.readInt(true, false);
+                long chunkUnknown2 = this.readInt(true, false);
 
                 // Number of Blocks
                 long blockCount = this.readInt(true, true);
+                List<PartBlock> partBlocks = new ArrayList<>();
                 for (int k = 0; k < blockCount; k++) {
-                    // Block Data
-                    this.seek(9, false);
+                    int frameOffset = this.readShort(true, false);
+                    int unknownId1 = this.readUnsignedByte();
+                    int unknownId2 = this.readUnsignedByte();
+                    int unknownId3 = this.readUnsignedByte();
+                    int unknownId4 = this.readUnsignedByte();
+                    int unknownId5 = this.readUnsignedByte();
+                    int unknownId6 = this.readUnsignedByte();
+                    int unknownId7 = this.readUnsignedByte();
+                    PartBlock block = new PartBlock(frameOffset, unknownId1, unknownId2, unknownId3, unknownId4,
+                            unknownId5, unknownId6, unknownId7);
+                    partBlocks.add(block);
                 }
+                PartChunk chunk = new PartChunk((int)chunkNumber, (int)chunkUnknown2, partBlocks);
+                partChunks.add(chunk);
             }
+            Part part = new Part(id, paletteIndex, frameIndex, frameCount, partChunks);
+            this.parts.add(part);
         }
 
         this.close();
