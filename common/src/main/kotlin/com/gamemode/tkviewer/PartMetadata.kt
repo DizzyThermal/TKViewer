b76962f7a98405e2e0087a613b5f443944c492b8
Stephen Diniz <DizzyThermal@gmail.com>
2020-09-04 16:53:46 -0400

    Migrating

--- common/src/main/kotlin/com/gamemode/tkviewer/PartMetadata.kt
+++ common/src/main/kotlin/com/gamemode/tkviewer/PartMetadata.kt
@@ -1,5 +1,4 @@
 package com.gamemode.tkviewer
 
-class PartMetadata(val unknownPart1: Integer, val unknownPart2: Integer, val unknownPart3: Integer,
-                   val unknownPart4: Integer, val unknownPart5: Integer, val unknownPart6: Integer,
-                   val unknownPart7: Integer)
+class PartMetadata(val unknownPart1: Int, val unknownPart2: Int, val unknownPart3: Int, val unknownPart4: Int,
+                   val unknownPart5: Int, val unknownPart6: Int, val unknownPart7: Int)
